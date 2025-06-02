340a24bcab9e045663de303379e08afb9150aa95
Max <maxateh6@gmail.com>
2025-06-02 19:24:34 +0100

    queries and docs updates

---
+++ queries/rankings.sql
@@ -0,0 +1,108 @@
+/* 
+Question: What are the top paying data analyst jobs
+- Identify the top ten highest paying data analyst roles that are available remotely
+- focus on Focus on job postings with specified salaries (remove nulls)
+- Why? Highlight the top paying opportunities for data analysts, offering insights into 
+*/
+
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC;
+
+
+/* 
+Question: What are the top paying data analyst jobs
+- Why? It provides a detailed look at which high-paying jobs demand certain skills
+- Helping job seekers to understand what skills to develop that align with high-paying salaries
+*/;
+
+
+WITH top_paying_jobs AS (
+    SELECT 
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM
+        job_postings_fact
+        LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+)
+
+SELECT 
+    top_paying_jobs.*
+    Skills
+FROM
+    top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC;
+
+
+
+/* 
+Question: What are the most in-demand skills for data analyst?
+- Providing insights to the most valuable skills for job seekers.
+*/;
+
+
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+GROUP BY
+    SKILLS
+ORDER BY
+    demand_count DESC
+LIMIT 10;
+
+
+
+/* What are the top skills based on salary?
+
+It reveals how different skills impact salary levels for data analysts and
+helps identify the most financially rewarding skills to acquire and improve.
+*/
+
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 30;
+
+
